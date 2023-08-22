5506306204fc3de7978dc566f4213272c17e87f4
Emily Traynor <emily.traynor@mail.utoronto.ca>
2023-08-21 20:31:58 -0400

    first commit

---
+++ GurobiAssistant/src/main.rs
@@ -0,0 +1,9 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+
+
+fn main() {
+    // launch the dioxus app in a webview
+    let expression = r#"y=\frac{1}{x}"#;
+    
+}
